7d34419b816de84778c8631fe6b70cf76ed7edce
mostafa-elrosasy <mostafaelrosasy.1998@gmail.com>
2019-12-29 01:39:22 +0200

    skybox and control

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Create Camera And Scene
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(35,window.innerWidth/window.innerHeight,0.1,1000)
-camera.position.z = 2;
+var camera = new THREE.PerspectiveCamera(55,window.innerWidth/window.innerHeight,0.1,1500)
+camera.position.z = 50;
 
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setClearColor("#e5e5e5");
@@ -10,10 +10,41 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+// Control
+let controls = new THREE.OrbitControls(camera);
+controls.addEventListener('change', renderer);
+controls.minDistance = 50;
+controls.maxDistance = 250;
+
+// Sky Box
+let materialArray = [];
+
+let texture_ft = new THREE.TextureLoader().load( 'sky_box/yonder_ft.jpg');
+let texture_bk = new THREE.TextureLoader().load( 'sky_box/yonder_bk.jpg');
+let texture_up = new THREE.TextureLoader().load( 'sky_box/yonder_up.jpg');
+let texture_dn = new THREE.TextureLoader().load( 'sky_box/yonder_dn.jpg');
+let texture_rt = new THREE.TextureLoader().load( 'sky_box/yonder_rt.jpg');
+let texture_lf = new THREE.TextureLoader().load( 'sky_box/yonder_lf.jpg');
+  
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+   
+for (let i = 0; i < 6; i++)
+  materialArray[i].side = THREE.BackSide;
+   
+let skyboxGeo = new THREE.BoxGeometry( 500, 500, 500);
+let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+scene.add(skybox);
+
+
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth,window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
-
+    
     camera.updateProjectionMatrix();
 })
 
@@ -59,13 +90,13 @@ scene.add(light);
 
 //Animation Function
 var render = function() {
-    requestAnimationFrame(render);
-    Goku.rotation.z += 0.01;
-    Goku2.rotation.z += 0.01;
+    // Goku.rotation.z += 0.01;
+    // Goku2.rotation.z += 0.01;
     //Goku2.rotation.y += 0.01;
    // Goku2.rotation.x += 0.01;
+   renderer.render(scene, camera);
+   requestAnimationFrame(render);
 
-    renderer.render(scene, camera);
 }
 
 
